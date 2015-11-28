0696b97de07c24ace3743b4f9a8ce1141c6cdf52
unicorn <user@gmail.com>
2015-11-28 14:42:03 +0100

    neues Projekt

---
+++ CollaborativeTrustcase/App-Night/app/src/main/java/com/example/unicorn/collaborativetrustcase/School.java
@@ -0,0 +1,11 @@
+package com.example.unicorn.collaborativetrustcase;
+
+/**
+ * Created by unicorn on 28.11.15.
+ */
+public class School {
+
+
+    private String schoolName, schoolDesription;
+    private 
+}
