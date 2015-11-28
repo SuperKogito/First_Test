e9e6bb5fb8fad92ebaa716708c20dc61ac05e121
unicorn <user@gmail.com>
2015-11-28 18:27:15 +0100

    kdjfaskjl

--- CollaborativeTrustcase/Collaborative/app/src/main/java/com/example/unicorn/collaborative/DisplaySchoolClassesActivity.java
+++ CollaborativeTrustcase/Collaborative/app/src/main/java/com/example/unicorn/collaborative/DisplaySchoolClassesActivity.java
@@ -2,6 +2,7 @@ package com.example.unicorn.collaborative;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
@@ -45,20 +46,6 @@ public class DisplaySchoolClassesActivity extends Activity {
         mListView = (AbsListView) findViewById(R.id.schools_list);
 
 
-        try {
-            List<RoomListResponseItem> classes = getSchoolClasses();
-            for (RoomListResponseItem item: classes)
-            {
-
-
-                Log.d("TAG", item.getId());
-
-
-            }
-
-        } catch (TrustCaseClientException e) {
-            e.printStackTrace();
-        }
 
 
 
@@ -91,19 +78,54 @@ public class DisplaySchoolClassesActivity extends Activity {
             }
         });
 
+        new AsyncTask<Object,Void, List>() {
+            @Override
+            protected List doInBackground(Object[] params) {
+                final String endpoint = "https://api.trustcase.com/api/v0.6";
+                final String jid = "6MB5S98G";
+                final String privateKey = "6ebb05d98e7c34ddcdc77341da2b5581478438ef2ca3cf9badcd6f20f8ae6872";
+                final String publicKey = "2be5f7bde50463b3e8b42647e0e7582c0c8f5a399aa7f1ac1604d2b298402e46";
+                final String password = "88e66feeb4eb49fe95bfaced46106f7e";
+                TrustCaseClient client = TrustCase.newBuilder().apiEndpoint(endpoint).jid(jid).privateKey(privateKey)
+                        .publicKey(publicKey).password(password).build();
+
+                List rooms=null;
+                try {
+                 rooms  = client.listRooms();
+                } catch (TrustCaseClientException e) {
+                    e.printStackTrace();
+                }
+
+                return rooms;
+
+
+            }
+
+            @Override
+            protected void onPostExecute(List o) {
+
+
+                    List<RoomListResponseItem> classes = o;
+                    for (RoomListResponseItem item: classes)
+                    {
+
+
+                        Log.d("TAG", "id:" +item.getId());
+                        Log.d("TAG", "sonstiges: "+item.getLastMessageBy());
+
+
+                    }
+
+
+
+
+             //   super.onPostExecute(o);
+            }
+        }.execute();
+
     }
 
 
-    public List<RoomListResponseItem> getSchoolClasses() throws TrustCaseClientException {
-        final String endpoint = "https://api.trustcase.com/api/v0.6";
-        final String jid = "6MB5S98G";
-        final String privateKey = "6ebb05d98e7c34ddcdc77341da2b5581478438ef2ca3cf9badcd6f20f8ae6872";
-        final String publicKey = "2be5f7bde50463b3e8b42647e0e7582c0c8f5a399aa7f1ac1604d2b298402e46";
-        final String password = "88e66feeb4eb49fe95bfaced46106f7e";
-        TrustCaseClient client = TrustCase.newBuilder().apiEndpoint(endpoint).jid(jid).privateKey(privateKey)
-                .publicKey(publicKey).password(password).build();
-        return client.listRooms();
 
-    }
 
 }
